f446e81a09e352e73a33ee3cd163ab86847f79ea
wudong1997 <62734556+wuhtung@users.noreply.github.com>
2021-06-25 15:16:07 +0900

    Initial commit

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
 import numpy as np
+import numpy.ma
 
-T = np.array([9, 15, 25, 14, 10, 18, 0, 16, 5, 19, 16, 20])
-S = np.array([39, 56, 93, 61, 50, 75, 32, 85, 42, 70, 66, 80])
-T = T[:, np.newaxis]
-S = S[:, np.newaxis]
-X = np.hstack((T, S))
-print(X)
-figValue, figVec = np.linalg.eig(np.cov(X.T))
+import readRSImg
+import rs_index
+import show_pic
+import pca
 
-print(figValue)
+
+if __name__ == '__main__':
+
+    ls8 = readRSImg.landsat8()
+    img = ls8.read_envi(path='E:\\RSImg\\pca_test.tif')
+    ndwi_img = rs_index.ndwi(img)
+
+    mask = np.full((img.shape[0], img.shape[1], img.shape[2]), True)
+
+    band_mask = ndwi_img > 0.1
+    for i in range(img.shape[2]):
+        mask[:, :, i] = band_mask
+
+    img = numpy.ma.array(img, mask=mask)
+
+    out = pca.pca(img)
+
+    show_pic.show_single_band(out[:, :, 1])
+
+
