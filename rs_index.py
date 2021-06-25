ccb148998049850a152acdcd43cfa0de305e0354
wudong1997 <62734556+wuhtung@users.noreply.github.com>
2021-06-25 15:12:08 +0900

    1.0

--- rs_index.py
+++ rs_index.py
@@ -19,18 +19,3 @@ def ndwi(img):
 
     NDWI = (green - nir) / (green + nir)
     return NDWI
-
-
-def otsu(gray_img):
-    ret1, th1 = cv2.threshold(gray_img, thresh=0.1, maxval=1, type=cv2.THRESH_BINARY)
-    show_pic.show_single_band(gray_img)
-    show_pic.draw_hist(gray_img)
-    plt.figure(dpi=300)
-    plt.show()
-
-
-ls8 = readRSImg.landsat8()
-img = ls8.read_envi('E:\\RSImg\\atmos')
-ndwi_img = ndvi(img)
-
-otsu(ndwi_img)
